fcd5a3e9ed6e790bbbcc36ebdcdbe4c48366ac90
andrewroberts <andrew@roberts.net>
2014-07-06 20:00:25 +0100

    Winning logic in

--- ttt.js
+++ ttt.js
@@ -32,8 +32,7 @@ function BoardClass() {
 	this.init = function() {
 		
 		var i,
-			cell,
-			NUMBER_CELLS = 9;
+			cell;
 		
 		// Initialise the board array.
 		for (i = 0, board.length = 0; i < NUMBER_CELLS; i++) {
@@ -77,7 +76,7 @@ function BoardClass() {
 
 		if (board[i] === PLAYER.CROSS || board[i] === PLAYER.NOUGHT) {
 	
-			out('The move has already been taken');
+			out('That move has already been taken');
 			return false;
 		}
 
@@ -97,7 +96,127 @@ function BoardClass() {
 		// Look for a winning move
 		// -----------------------
 
-		// TODO
+		// TODO - make into a function table.
+
+		// Check for '0 0 .' on top line, e.g. 1 and 2.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] === PLAYER.NOUGHT && 
+			    board[1] === PLAYER.NOUGHT && 
+			    board[2] !== PLAYER.CROSS && 
+			    !madeMove) {
+				
+				board[2] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Check for '. 0 0' on top line, e.g. 2 and 3.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] !== PLAYER.CROSS && 
+			    board[1] === PLAYER.NOUGHT && 
+			    board[2] === PLAYER.NOUGHT && 
+			    !madeMove) {
+				
+				board[0] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Check for '0 0 .' through the middle from the middle, e.g. 3 and 5.
+		for (i = 0; i < 4; i++) {
+
+			if (board[1] === PLAYER.NOUGHT && 
+				board[4] === PLAYER.NOUGHT && 
+				board[7] !== PLAYER.CROSS &&
+				!madeMove) {
+				
+				board[7] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+		
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Check for '0 0 .' through the middle from the corner, e.g. 1 and 5.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] === PLAYER.NOUGHT && 
+				board[4] === PLAYER.NOUGHT && 
+				board[8] !== PLAYER.CROSS &&
+				!madeMove) {
+				
+				board[8] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Check for '0 . 0' on the edges, e.g. 1 and 3.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] === PLAYER.NOUGHT && 
+				board[2] === PLAYER.NOUGHT && 
+				board[1] !== PLAYER.CROSS &&
+				!madeMove) {
+				
+				board[1] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Check for '0 . 0' through the middle, e.g. 1 and 9.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] === PLAYER.NOUGHT && 
+				board[4] !== PLAYER.CROSS &&
+				board[8] === PLAYER.NOUGHT && 
+				!madeMove) {
+				
+				board[4] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
 
 		// Look for a defensive, blocking move
 		// -----------------------------------
@@ -107,7 +226,10 @@ function BoardClass() {
 		// Check for 'X X .' on top line, e.g. 1 and 2.
 		for (i = 0; i < 4; i++) {
 
-			if (board[0] === PLAYER.CROSS && board[1] == PLAYER.CROSS && !madeMove) {
+			if (board[0] === PLAYER.CROSS && 
+			    board[1] === PLAYER.CROSS && 
+			    board[2] !== PLAYER.NOUGHT && 
+			    !madeMove) {
 				
 				board[2] = PLAYER.NOUGHT;
 				madeMove = true;
@@ -124,7 +246,10 @@ function BoardClass() {
 		// Check for '. X X' on top line, e.g. 2 and 3.
 		for (i = 0; i < 4; i++) {
 
-			if (board[1] === PLAYER.CROSS && board[2] == PLAYER.CROSS && !madeMove) {
+			if (board[0] !== PLAYER.NOUGHT && 
+			    board[1] === PLAYER.CROSS && 
+			    board[2] === PLAYER.CROSS && 
+			    !madeMove) {
 				
 				board[0] = PLAYER.NOUGHT;
 				madeMove = true;
@@ -138,10 +263,13 @@ function BoardClass() {
 			return;
 		}
 
-		// Check for 'X X .' through the middle from the middle, e.g. 3 and 5 
+		// Check for 'X X .' through the middle from the middle, e.g. 3 and 5.
 		for (i = 0; i < 4; i++) {
 
-			if (board[1] === PLAYER.CROSS && board[4] == PLAYER.CROSS && !madeMove) {
+			if (board[1] === PLAYER.CROSS && 
+				board[4] === PLAYER.CROSS && 
+				board[7] !== PLAYER.NOUGHT &&
+				!madeMove) {
 				
 				board[7] = PLAYER.NOUGHT;
 				madeMove = true;
@@ -155,10 +283,13 @@ function BoardClass() {
 			return;
 		}
 
-		// Check for 'X X .' through the middle from the corner, e.g. 1 and 5 
+		// Check for 'X X .' through the middle from the corner, e.g. 1 and 5.
 		for (i = 0; i < 4; i++) {
 
-			if (board[0] === PLAYER.CROSS && board[4] == PLAYER.CROSS && !madeMove) {
+			if (board[0] === PLAYER.CROSS && 
+				board[4] === PLAYER.CROSS && 
+				board[8] !== PLAYER.NOUGHT &&
+				!madeMove) {
 				
 				board[8] = PLAYER.NOUGHT;
 				madeMove = true;
@@ -172,26 +303,64 @@ function BoardClass() {
 			return;
 		}
 
-		// Put it in the middle if that's not  taken.
-		if (!moveTaken(4)) {
+		// Check for 'X . X' on the edges, e.g. 1 and 3.
+		for (i = 0; i < 4; i++) {
 
-			board[4] = PLAYER.NOUGHT;
-			madeMove = true;
-		}
+			if (board[0] === PLAYER.CROSS && 
+				board[2] === PLAYER.CROSS && 
+				board[1] !== PLAYER.NOUGHT &&
+				!madeMove) {
+				
+				board[1] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
 			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
 		if (madeMove) {
 			
 			return;
 		}
 
-		// TODO - Make this random. 
+		// Check for 'X . X' through the middle, e.g. 1 and 9.
+		for (i = 0; i < 4; i++) {
+
+			if (board[0] === PLAYER.CROSS && 
+				board[4] !== PLAYER.NOUGHT &&
+				board[8] === PLAYER.CROSS && 
+				!madeMove) {
+				
+				board[4] = PLAYER.NOUGHT;
+				madeMove = true;
+			}
+			
+			transform(TRANSFORM.ROTATE_90_CW);
+		}
+
+		if (madeMove) {
+			
+			return;
+		}
+
+		// Put it in the middle if that's not taken.
+		if (!cellUsed(4)) {
+
+			board[4] = PLAYER.NOUGHT;
+			madeMove = true;
+		}
+		
+		if (madeMove) {
+			
+			return;
+		}
+
+		// TODO - Make this random.
 
 		// Simply fill in the first free space.
-		for (i = 0; i < 9; i++) {
+		for (i = 0; i < NUMBER_CELLS; i++) {
 			
-			// TODO - Put this check in a function.
-			
-			if (!moveTaken(i)) {
+			if (!cellUsed(i)) {
 			
 				board[i] = PLAYER.NOUGHT;
 				madeMove = true;
@@ -207,15 +376,17 @@ function BoardClass() {
 	this.checkForWin = function() {
 
 		var i,
-			draw = true;
+			draw = true,
+			won = false;
 				
 		// Check for winning patterns across, by shifting the board in the board down 
 		// three times and see if the winning pattern comes into the first row.
 		for (i = 0; i < 3; i++) {
 
-			if (moveTaken(0) && board[0] === board[1] && board[0] === board[2]) {
+			if (cellUsed(0) && board[0] === board[1] && board[0] === board[2]) {
 			
 				win();
+				won = true;
 			}
 			
 			transform(TRANSFORM.SHIFT_DOWN);
@@ -223,11 +394,12 @@ function BoardClass() {
 		
 		// Check for winning pattern down, by shifting the cells in the board right 
 		// three times and see if the winning pattern comes into the first column.
-		for (i = 0; i < 3; i++) {
+		for (i = 0; i < 3 && !won; i++) {
 
-			if (moveTaken(0) && board[0] === board[3] && board[0] === board[6]) {
+			if (cellUsed(0) && board[0] === board[3] && board[0] === board[6]) {
 				
 				win();
+				won = true;				
 			}
 			
 			transform(TRANSFORM.SHIFT_RIGHT);			
@@ -236,37 +408,45 @@ function BoardClass() {
 		// Check for winning pattern diagonally by rotating the cells 90 degrees CW 
 		// three times and see if the winning pattern comes into the first column.
 		// The last ones aren't actually necessary, they just put things back as they were.
-		for (i = 0; i < 4; i++) {
+		for (i = 0; i < 4 && !won; i++) {
 
-			if (moveTaken(0) && board[0] === board[4] && board[0] === board[8]) {
+			if (cellUsed(0) && board[0] === board[4] && board[0] === board[8]) {
 				
 				win();
+				won = true;				
 			}
 			
 			transform(TRANSFORM.ROTATE_90_CW);
 		}
 
-		// Check for a draw
+		// If a win hasn't already been found check if the board is now full.
 		
-		for (i = 0; i < board.length; i++) {
+		for (i = 0; i < board.length && !won; i++) {
 			
-			if (!moveTaken(i)) {
+			if (!cellUsed(i)) {
 				
 				draw = false;
 				break;
 			}
 		}
 
-		if (draw) {
+		if (draw && !won) {
 			
 			out('\nDraw - resetting board');
 			instance.init();
 		}
 
+		return won || draw;
+
 		// Board.checkForWin.win()
 		// -----------------------
 
 		function win() {
+			
+			if (won) {
+				
+				throw new Error('Board.checkForWin.win() - already won');
+			}
 			
 			out(board[0] + ' wins!\n');
 			out('Pick a number from 1 to 9:');
@@ -283,7 +463,9 @@ function BoardClass() {
 	var board = [],
 
 	// Return the instance to make this a singleton.
-		instance = this;
+		instance = this,
+		
+		NUMBER_CELLS = 9;
 
 	// Board.transform()
 	// ----------------
@@ -327,10 +509,19 @@ function BoardClass() {
 		
 	} // Board.transform()
 	
-	function moveTaken(id) {
+	// Board.cellUsed()
+	// ----------------
+	
+	function cellUsed(i) {
 		
-		return (board[id - 1] === PLAYER.CROSS || board[id - 1] === PLAYER.NOUGHT);
-	}
+		if (i < 0 || i >= NUMBER_CELLS) {
+			
+			throw new Error('Board.cellUsed() - illegal index');
+		}
+		
+		return (board[i] === PLAYER.CROSS || board[i] === PLAYER.NOUGHT);
+		
+	} // Board.cellUsed()
 							
 } // Board()
 
@@ -362,13 +553,18 @@ stdin.on('data', function(key) {
 	
 	Board.display();
 	
-	Board.checkForWin(PLAYER.CROSS);
+	if (Board.checkForWin(PLAYER.CROSS))
+	{
+		return;
+	}
   
 	Board.getComputerResponse(key);
 
 	Board.display();
 
 	Board.checkForWin(PLAYER.NOUGHT);
+	
+	return;
 	
 	// Private
 	// -------
